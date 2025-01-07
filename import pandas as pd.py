50a5ab9282ef8dc29b55cf39f78676a565b7b05b
jhon-wlk <jhon.wilksom@gmail.com>
2025-01-06 21:34:07 -0300

    Atualizando script para ler o arquivo CSV corretamente

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 # Lendo o arquivo CSV
+data = pd.read_csv("vehicles_new.csv")
 data = pd.read_csv("C:/Users/Jhonw/OneDrive/Documents/GitHub/README/vehicles_new.csv")
 
 # Exibindo as primeiras linhas para verificar se carregou corretamente
